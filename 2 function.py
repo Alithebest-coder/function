fc0bd0a1bd435b1057fe80832d4f4df2b842a4ed
Alithebest-coder <aurumprint@proton.me>
2024-12-16 17:47:42 +1000

    Add files via upload

---
+++ 2 function.py
@@ -0,0 +1,5 @@
+def weather():
+    
+    print("The Weather is raining so do not go and play with a shot sheirt without a rain coat")
+    print("The Weather is sunny so please do no wear a thik woolly jumper")
+weather()
